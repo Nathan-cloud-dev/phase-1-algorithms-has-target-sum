c40611febbc5a5301e1e0f0afd9a52561be12018
Nathan <kiprotichnathan@yahoo.com>
2023-10-09 00:57:53 +0300

    task done

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function hasTargetSum(array, target) {
+function hasTargetSum(array, target)
+ {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++)
+  {
+    let firstNum = array[i];
+
+    let secondNum = target - firstNum;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === secondNum)
+       {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  nested loop - O(n*n) => O(n^2)
 */
 
 /* 
   Add your pseudocode here
+two random numbers in array is equal to target , return true
+else return false
+
 */
 
 /*
@@ -19,16 +39,14 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  //it has 3 and 7
 
-  console.log("");
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 3, 5], 4));
 }
 
 module.exports = hasTargetSum;
